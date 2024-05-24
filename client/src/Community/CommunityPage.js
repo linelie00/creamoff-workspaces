3ad858298dc6485ed4c8720574a4a506aa48cf7b
linelie00 <dingpawn@gmail.com>
2024-05-24 11:02:42 +0900

    [Feat]:버튼 추가 및 list 컨테이너 추가

--- client/src/Community/CommunityPage.js
+++ client/src/Community/CommunityPage.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './community.css';
 import '../font.css';
 
 const CommunityPage = () => {
+  const [nickname, setNickname] = useState('');
   const floatingButtonUrl = `${process.env.PUBLIC_URL}/images/floatingButton.svg`;
+
+
+  useEffect(() => {
+    const fetchNickname = async () => {
+      const fetchedNickname = '닉네임';
+      setNickname(fetchedNickname);
+    };
+
+    fetchNickname();
+  }, []);
+
+  const onProfilClick = () => {
+    window.location.href = '/edit-user';
+  }
 
   return (
     <div>
@@ -12,10 +27,23 @@ const CommunityPage = () => {
         </div>
         <div className='header'>
             <div className='header-nickname'>
-                <a href="/edit-user">닉네임</a>
+                <a href="/edit-user">{nickname}</a>
+                <div classNamm='header-nickname-button-container'>
+                    <button className='header-nickname-button' onClick={onProfilClick}>
+                        <img src={`${process.env.PUBLIC_URL}/images/profil.svg`} alt="profil" />
+                    </button>
+                    <button className='header-nickname-button'>
+                        <img src={`${process.env.PUBLIC_URL}/images/Trailing.svg`} alt="profil" />
+                    </button>
+                    <button className='header-nickname-button'>
+                        <img src={`${process.env.PUBLIC_URL}/images/ic.svg`} alt="profil" />
+                    </button>
+                </div>
             </div>
             <div className='header-bar'>
             </div>
+        </div>
+        <div className='list'>
         </div>
         <div className='navigation-bar'></div>
         <button className='floating-button'>
