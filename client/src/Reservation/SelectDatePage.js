5d3b8f6100d3517bcb2f8ecd4f03e26d64ceaec5
linelie00 <dingpawn@gmail.com>
2024-06-24 09:32:22 +0900

    일정 선택 디자인

--- client/src/Reservation/SelectDatePage.js
+++ client/src/Reservation/SelectDatePage.js
@@ -5,6 +5,7 @@ const Review = () => {
     const navigate = useNavigate();
     const { id } = useParams(); // URL에서 이벤트 ID 가져오기
     const arrowButtonUrl = `${process.env.PUBLIC_URL}/images/list/arrow_left.svg`;
+    const calenderUrl = `${process.env.PUBLIC_URL}/images/list/calender.svg`;
 
     const goBack = () => {
         navigate(-1); // 뒤로 가기
@@ -21,6 +22,23 @@ const Review = () => {
                     <div></div>
                 </div>
                 <div className='review-mid'>
+                    <div className='schedule-container'>
+                        <div className='schedule-contents'>
+                            <div className='schedule'>
+                                2024/03/04
+                                <button>
+                                    <img src={calenderUrl} alt=''/>
+                                </button>
+                            </div>
+                        </div>
+                        <div className='schedule-contents'>
+                            <div className='schedule'>
+                                <div>오후</div>
+                                <div>12시</div>
+                                <div>30분</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='Nbutton' onClick={() => navigate('/reservation-confirm')}>예약등록</div>
