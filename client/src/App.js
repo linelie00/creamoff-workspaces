e7badb98bd7a45584b437f8739023456f2910c6f
KMS322 <kms930322@naver.com>
2024-09-23 22:59:30 +0900

    add privacy component

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,82 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './Home/HomePage';
-import NoticePage from './Notice/NoticePage';
-import ReviewPage from './Notice/ReviewPage';
-import ViewDetailsPage from './Notice/ViewDetailsPage';
-import ListPage from './List/ListPage';
-import ListMapPage from './List/ListMapPage';
-import CommunityPage from './Community/CommunityPage';
-import EditUserPage from './EditUser/EditUserPage';
-import EditAdressPage from './EditUser/EditAddressPage';
-import MyPage from './MyPage/MyPage';
-import SavedPage from './MyPage/SavedPage';
-import EventDetailPage from './List/EventDetailPage'; 
-import LoginPage from './Login/LoginPage';
-import KoginSuccess from './Login/LoginSuccessPage';
-import Redirection from './Login/Redirection';
-import PetListPage from './Pet/PetListPage';
-import PetSelectPage from './Pet/PetSelectPage';
-import PetRegistrationPage from './Pet/PetRegistrationPage';
-import ReservationRequestPage from './Reservation/ReservationRequestPage';
-import SelectDatePage from './Reservation/SelectDatePage';
-import ReservatinConfirm from './Reservation/ReservationConfirmPage';
-import ReservationPage from './Reservation/ReservationPage';
-import CancleReservationPage from './Reservation/CancleReservationPage';
-import UploadPage from './File/UploadPage';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Home/HomePage";
+import NoticePage from "./Notice/NoticePage";
+import ReviewPage from "./Notice/ReviewPage";
+import ViewDetailsPage from "./Notice/ViewDetailsPage";
+import ListPage from "./List/ListPage";
+import ListMapPage from "./List/ListMapPage";
+import CommunityPage from "./Community/CommunityPage";
+import EditUserPage from "./EditUser/EditUserPage";
+import EditAdressPage from "./EditUser/EditAddressPage";
+import MyPage from "./MyPage/MyPage";
+import SavedPage from "./MyPage/SavedPage";
+import EventDetailPage from "./List/EventDetailPage";
+import LoginPage from "./Login/LoginPage";
+import KoginSuccess from "./Login/LoginSuccessPage";
+import Redirection from "./Login/Redirection";
+import PetListPage from "./Pet/PetListPage";
+import PetSelectPage from "./Pet/PetSelectPage";
+import PetRegistrationPage from "./Pet/PetRegistrationPage";
+import ReservationRequestPage from "./Reservation/ReservationRequestPage";
+import SelectDatePage from "./Reservation/SelectDatePage";
+import ReservatinConfirm from "./Reservation/ReservationConfirmPage";
+import ReservationPage from "./Reservation/ReservationPage";
+import CancleReservationPage from "./Reservation/CancleReservationPage";
+import UploadPage from "./File/UploadPage";
+import PrivacyPolicy from "./PrivacyPolicy/privacyPolicy";
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-          <div className="Container">
-            <Router>
-              <Routes>
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/list/:id" element={<ListPage />} />
-                <Route path="/community" element={<CommunityPage />} />
-                <Route path="/edit-user" element={<EditUserPage />} />
-                <Route path="/my-page" element={<MyPage />} />
-                <Route path="/list-map/:id" element={<ListMapPage />} />
-                <Route path="/events/:id" element={<EventDetailPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/pet-list" element={<PetListPage />} />
-                <Route path="/notice" element={<NoticePage />} />
-                <Route path="/events/:id/review" element={<ReviewPage />} />
-                <Route path="/events/:id/view-details" element={<ViewDetailsPage />} />
-                <Route path="/pet-select/:id" element={<PetSelectPage />} />
-                <Route path="/reservation-request/:id" element={<ReservationRequestPage />} />
-                <Route path="/select-date" element={<SelectDatePage />} />
-                <Route path="/reservation-confirm" element={<ReservatinConfirm />} />
-                <Route path="/reservation" element={<ReservationPage />} />
-                <Route path="/cancle-reservation" element={<CancleReservationPage />} />
-                <Route path="/pet-registration" element={<PetRegistrationPage />} />
-                <Route path="/saved" element={<SavedPage />} />
-                <Route path="/auth/*" element={<Redirection />} />
-                <Route path="/login-success" element={<KoginSuccess />} />
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/edit-address" element={<EditAdressPage />} />
-                <Route path="/upload" element={<UploadPage />} />
-                {/* 다른 라우트들 추가 */}
-              </Routes>
-            </Router>
-          </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="Container">
+        <Router>
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/list/:id" element={<ListPage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/edit-user" element={<EditUserPage />} />
+            <Route path="/my-page" element={<MyPage />} />
+            <Route path="/list-map/:id" element={<ListMapPage />} />
+            <Route path="/events/:id" element={<EventDetailPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/pet-list" element={<PetListPage />} />
+            <Route path="/notice" element={<NoticePage />} />
+            <Route path="/events/:id/review" element={<ReviewPage />} />
+            <Route
+              path="/events/:id/view-details"
+              element={<ViewDetailsPage />}
+            />
+            <Route path="/pet-select/:id" element={<PetSelectPage />} />
+            <Route
+              path="/reservation-request/:id"
+              element={<ReservationRequestPage />}
+            />
+            <Route path="/select-date" element={<SelectDatePage />} />
+            <Route
+              path="/reservation-confirm"
+              element={<ReservatinConfirm />}
+            />
+            <Route path="/reservation" element={<ReservationPage />} />
+            <Route
+              path="/cancle-reservation"
+              element={<CancleReservationPage />}
+            />
+            <Route path="/pet-registration" element={<PetRegistrationPage />} />
+            <Route path="/saved" element={<SavedPage />} />
+            <Route path="/auth/*" element={<Redirection />} />
+            <Route path="/login-success" element={<KoginSuccess />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/edit-address" element={<EditAdressPage />} />
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+            {/* 다른 라우트들 추가 */}
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
 }
 
 export default App;
